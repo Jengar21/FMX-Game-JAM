ec662a12e4139cb142b60ece09062a3e37bade06
jenngarcia <arisjennifer21@gmail.com>
2025-05-03 18:09:12 +0200

    function to load level 1

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
-fetch('levels/level1.html')
-    .then(response => response.text())
-    .then(html => {
-      document.getElementById('post-board-container').innerHTML = html;
-    })
-    .catch(error => console.error('Error loading post board:', error));
+function loadLevel1() {
+  fetch('levels/level1.html')
+      .then(response => response.text())
+      .then(html => {
+          document.getElementById('post-board-container').innerHTML = html;
+          console.log('Level 1 loaded.');
+      })
+      .catch(error => console.error('Error loading level 1:', error));
+}
